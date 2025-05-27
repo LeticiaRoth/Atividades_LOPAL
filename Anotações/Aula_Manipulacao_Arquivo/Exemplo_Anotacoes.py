74f8a9609db5a82ca922a53451353d0d2d61be0e
LeticiaRoth <leticiaroth1@gmail.com>
2025-05-27 15:11:12 -0300

    Atualização

---
+++ Anotações/Aula_Manipulacao_Arquivo/Exemplo_Anotacoes.py
@@ -0,0 +1,60 @@
+import csv
+
+with open("meu_arquivo.txt", "w") as arquivo:
+    arquivo.write("Olá mundo, eu sou Leticia Roth!\n")
+    arquivo.write("Aprendendo Python!\n")
+
+
+#Abre o arquivo em modo de leitura, "r"
+arquivo = open("meu_arquivo.txt", "r")
+#Lê o conteúdo do arquivo
+resultado = arquivo.read()
+print(f"Modo 1 \n{resultado}")
+#reusltado = arquivo.readlines() - lê com tudo na linha
+
+
+#Colocar caminho se não estiver dentro da pasta
+with open("meu_arquivo.txt", "r") as arquivo:
+    arquivo = open("meu_arquivo.txt", "r")
+    conteudo = arquivo.read()
+    print(f"Modo 2 \n{conteudo}")
+
+
+#Outra maneira de printar
+with open("meu_arquivo.txt", "r") as arquivo:
+    print(f"Modo 3 \n{arquivo.read()}")
+
+"""
+with open("produtos.csv", "w", newline='') as csvfile:
+    escrever = csv.writer(produto)
+    escrever.writerow("Nome, Preço")
+    escrever.writerow("Livro,Row")
+    for row in produto:
+        escrever.writerow(produto)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
